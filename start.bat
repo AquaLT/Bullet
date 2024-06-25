ab9484333a8ea3db2f09d33eafd42ab6f52842dc
Aqua <151952016+AquaLT@users.noreply.github.com>
2024-06-25 09:24:18 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
-START util\setup.exe
+START util\installer.exe
 title [+] Requirements 
 echo Preparing requirements
 pip install -r requirements.txt
-title [+] Bullet - Made By bullet.org
+title [+] Bullet - Made By Eagles
 python main.py
